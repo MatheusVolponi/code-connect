a7de10d714d2a4e9dfe2474b27b858f650cc8165
MatheusVolponi <matheusponath20@gmail.com>
2025-09-09 11:02:09 -0300

    feat: finalização de elementos no styles.css

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const uploadBtn = document.getElementById("upload-btn");
+const uploadImg = document.getElementById("upload-img");
+
+uploadBtn.addEventListener("click", () => {
+    uploadImg.click();
+});
